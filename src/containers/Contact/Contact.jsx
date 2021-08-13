380db9a995f2f420097151329b1e9d136af1aa28
José Luis <hansokaz@gmail.com>
2021-08-13 19:32:38 +0200

    contact style finished

--- src/containers/Contact/Contact.jsx
+++ src/containers/Contact/Contact.jsx
@@ -1,12 +1,28 @@
 
 import React from 'react';
+import linkedin from '../../img/Linkedin-símbolo.png'
 
 const Contact = () => {
 
-    return(
+    return (
         <div className="viewContact">
-            Hello, this is Contact
+
+
+            <form name="form">
+
+                <input class="text" name="contact" id="nombreContact" required="true" placeholder="Name"></input>
+
+                <input class="text" name="contact" id="emailContact" placeholder="Email"></input>
+
+
+                <textarea class="text textMessage" name="contact" id="textContact" placeholder="Your message"></textarea>
+
+                <button class="send" name="enviar" id="enviarContact">Send</button>
+            </form>
+
+            <img className="linklogo" src={linkedin}/>
         </div>
+
     )
 }
 
